7848a253bfeaaf155070c633f5a39bdb539d8ebe
asher-lab <asher01000001@gmail.com>
2022-12-29 20:52:04 +0800

    push code

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -7,6 +7,16 @@ type Props = {};
 // eslint-disable-next-line no-empty-pattern
 export default function HomePage({}: Props) {
   const [open, setOpen] = useState(true);
+  const Menus = [
+    { title: 'Dashboard', src: 'Chart_fill' },
+    { title: 'Inbox', src: 'Chat' },
+    { title: 'Accounts', src: 'User', gap: true },
+    { title: 'Schedule ', src: 'Calendar' },
+    { title: 'Search', src: 'Search' },
+    { title: 'Analytics', src: 'Chart' },
+    { title: 'Files ', src: 'Folder', gap: true },
+    { title: 'Setting', src: 'Setting' },
+  ];
   return (
     <div className="flex">
       <div
@@ -22,7 +32,9 @@ export default function HomePage({}: Props) {
         />
         <div className="flex flex-row items-center gap-x-4">
           <FaHome
-            className={`h-10 w-10 cursor-pointer bg-white duration-300`}
+            className={`h-10 w-10 cursor-pointer bg-white duration-300 ${
+              open && 'rotate-[360deg]'
+            }`}
           />
           <h1
             className={`origin-left text-xl font-medium text-white duration-300 ${
@@ -32,6 +44,21 @@ export default function HomePage({}: Props) {
             Designer
           </h1>
         </div>
+        <ul className="pt-6">
+          {Menus.map((menu, index) => (
+            <li
+              key={index}
+              className={`flex cursor-pointer items-center gap-x-4 rounded-md p-2 text-sm text-gray-300 hover:bg-light-white ${
+                menu.gap ? 'mt-9' : 'mt-2'
+              }`}
+            >
+              <img src={`/assets/${menu.src}.png`} alt="menuIcons" />
+              <span className={`${!open && 'hidden'}`} origin-left duration-200>
+                {menu.title}
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
 
       <div className="h-screen flex-1 p-7 text-2xl font-semibold">
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
     extend: {
       colors: {
         'dark-purple': '#081A51',
-        'light-white': 'rgba(255,255,255,0,0.17)',
+        'light-white': 'rgba(255,255,0,0.17)',
         gray: {
           100: '#f7fafc',
           200: '#edf2f7',
